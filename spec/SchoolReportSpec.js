da5de06518cbea6b0ed0fc484300d224e988b35d
Katie McDonagh <mcdonagh.katie@outlook.com>
2020-08-12 20:23:14 +0100

    red test -returns 'green: 1' when given 'green

--- spec/SchoolReportSpec.js
+++ spec/SchoolReportSpec.js
@@ -1,3 +1,10 @@
 describe("schoolReport", function(){
-  
+  let schoolReport
+  beforeEach(function() {
+    schoolReport = new SchoolReport
+  });
+
+    it ("returns 'green: 1' when given 'green'", function (){
+      expect(schoolReport.results("green")).toBe("green: 1")
+    })
 })
--- src/SchoolReport.js
+++ src/SchoolReport.js
@@ -1,7 +1,5 @@
-class schoolReport {
+class SchoolReport {
   constructor(){
-
   }
-  
 }
 
